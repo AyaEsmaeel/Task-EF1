af8d766e6cf83b2e7828a6ae6c612cb6abcc7282
AyaEsmaeel <115806612+AyaEsmaeel@users.noreply.github.com>
2024-05-20 12:06:52 +0300

    Add files via upload

---
+++ P02_SalesDatabase/Program.cs
@@ -0,0 +1,15 @@
+﻿using P02_SalesDatabase.Data;
+using P02_SalesDatabase.Models;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Customers customers = new Customers();
+        Products products = new Products();
+        Stores stores = new Stores();   
+        Sales sales = new Sales();
+        SalesContext salesContext = new SalesContext();
+
+    }
+}
